d5e5c647a2ba870520f6b3d6660f7854bc7ed716
Colin Phillips <colinp@discovery.co.za>
2018-08-26 15:35:35 +0200

    Changed the Sudoku obect into a struct with a pretty print method
    Now it looks awesome!
    Problem: current implementation takes 39 seconds and does 6GiB of memory allocations - can this be improved?

--- SudokuSolver.jl
+++ SudokuSolver.jl
@@ -9,12 +9,26 @@ mutable struct Cell
     boxid::Integer
     index::Integer
     value
-    Cell(domain, rowid, colid, index, value) = begin
+    known::Bool
+    fixed::Bool
+    Cell(domain, rowid, colid, index, value, known, fixed) = begin
         boxrow = ((rowid-1) ÷ 3) + 1
         boxcol = ((colid-1) ÷ 3) + 1
         boxid = boxcol + 3(boxrow - 1)
-        new(domain, rowid, colid, boxid, index, value)
+        new(domain, rowid, colid, boxid, index, value, known, fixed)
     end 
+end
+
+printcell(cell::Cell) = begin
+    if length(cell.domain) == 0
+        print_with_color(:red, "\u058D")
+    elseif !cell.fixed
+        print_with_color(:cyan, "\u0482")
+    elseif cell.known
+        print_with_color(:green, cell.value, bold = true)
+    else
+        print_with_color(:yellow, cell.value)
+    end
 end
 
 puzzle = [
@@ -29,47 +43,98 @@ puzzle = [
     0 9 0 0 0 0 4 0 0
 ];
 
-sudoku = Dict{String, Dict{Integer, Dict{Tuple{Integer, Integer}, Cell}}}(
-    "rows" => Dict{Integer, Dict{Tuple{Integer, Integer}, Cell}}(i => Dict{Tuple{Integer, Integer}, Cell}() for i ∈ 1:9),
-    "columns" => Dict{Integer, Dict{Tuple{Integer, Integer}, Cell}}(i => Dict{Tuple{Integer, Integer}, Cell}() for i ∈ 1:9),
-    "boxes" => Dict{Integer, Dict{Tuple{Integer, Integer}, Cell}}(i => Dict{Tuple{Integer, Integer}, Cell}() for i ∈ 1:9));
-
-linearindex = 1;                                               
-for (i, val) ∈ enumerate(IndexCartesian(), puzzle)
-    row = i[1];
-    col = i[2];
-    if val == 0
-        domain = Set(1:9);
-        value = nothing;
-    else
-        domain = Set(val);
-        value = val;
+struct Sudoku
+    rows::Dict{Integer, Dict{Tuple{Integer, Integer}, Cell}}
+    columns::Dict{Integer, Dict{Tuple{Integer, Integer}, Cell}}
+    boxes::Dict{Integer, Dict{Tuple{Integer, Integer}, Cell}}
+    Sudoku(puzz) = begin
+        rows = Dict{Integer, Dict{Tuple{Integer, Integer}, Cell}}(i => Dict{Tuple{Integer, Integer}, Cell}() for i ∈ 1:9)
+        columns = Dict{Integer, Dict{Tuple{Integer, Integer}, Cell}}(i => Dict{Tuple{Integer, Integer}, Cell}() for i ∈ 1:9)
+        boxes = Dict{Integer, Dict{Tuple{Integer, Integer}, Cell}}(i => Dict{Tuple{Integer, Integer}, Cell}() for i ∈ 1:9)
+        linearindex = 1;                                               
+        for (i, val) ∈ enumerate(IndexCartesian(), puzzle)
+            row = i[1];
+            col = i[2];
+            if val == 0
+                domain = Set(1:9);
+                value = nothing;
+                known = false;
+                fixed = false;
+            else
+                domain = Set(val);
+                value = val;
+                known = true;
+                fixed = true;
+            end
+            a = Cell(domain, row, col, linearindex, value, known, fixed);
+            push!(rows[row], (row, col) => a);
+            push!(columns[col], (row, col) => a);
+            push!(boxes[a.boxid], (row, col) => a);
+            linearindex += 1;
+        end
+        new(rows, columns, boxes)
     end
-    a = Cell(domain, row, col, linearindex, value);
-    push!(sudoku["rows"][row], (row, col) => a);
-    push!(sudoku["columns"][col], (row, col) => a);
-    push!(sudoku["boxes"][a.boxid], (row, col) => a);
-    linearindex += 1;
 end
 
-check = function(state)
+Base.show(io::IO, s::Sudoku) = begin
+    horiz = "—"^24
+    for r ∈ 1:9
+        row = s.rows[r]
+        if mod(r,3) == 1
+            println(horiz)
+        end
+        for c ∈ 1:9
+            cell = s.rows[r][(r,c)]
+            if mod(c,3) == 1
+                print("| ")
+            end
+            printcell(cell)
+            print(" ")
+        end
+        print("|\n")
+    end
+    println(horiz)
+end
+
+# sudoku = Dict{String, Dict{Integer, Dict{Tuple{Integer, Integer}, Cell}}}(
+#     "rows" => Dict{Integer, Dict{Tuple{Integer, Integer}, Cell}}(i => Dict{Tuple{Integer, Integer}, Cell}() for i ∈ 1:9),
+#     "columns" => Dict{Integer, Dict{Tuple{Integer, Integer}, Cell}}(i => Dict{Tuple{Integer, Integer}, Cell}() for i ∈ 1:9),
+#     "boxes" => Dict{Integer, Dict{Tuple{Integer, Integer}, Cell}}(i => Dict{Tuple{Integer, Integer}, Cell}() for i ∈ 1:9));
+
+# linearindex = 1;                                               
+# for (i, val) ∈ enumerate(IndexCartesian(), puzzle)
+#     row = i[1];
+#     col = i[2];
+#     if val == 0
+#         domain = Set(1:9);
+#         value = nothing;
+#     else
+#         domain = Set(val);
+#         value = val;
+#     end
+#     a = Cell(domain, row, col, linearindex, value);
+#     push!(sudoku["rows"][row], (row, col) => a);
+#     push!(sudoku["columns"][col], (row, col) => a);
+#     push!(sudoku["boxes"][a.boxid], (row, col) => a);
+#     linearindex += 1;
+# end
+
+update = function(state::Sudoku)
     # for each row, column, and box, remove the fixed values from the domains of all cells
     # for any domain of length 1, set the value to that element
     # if any domain is empty, return an empty array
     # otherwise return an array containing a new puzzle
-    soln = Array{Dict{String, Dict{Integer,Dict{Tuple{Integer, Integer}, Cell}}},1}();
     s = deepcopy(state); # make a copy so that we can freely change it
-    for (viewkey, view) ∈ s
+    for view ∈ [s.rows, s.columns, s.boxes];
         # viewkey is "rows", "columns", or "boxes"
         # view is a dictionary of a dict of cells (row, (row, col))
-        for (bunch, my_dict) ∈ view
-            for (cellkey, cell) ∈ my_dict
-                if length(cell.domain) == 0
-                    return soln #empty array since no possible solutions
-                elseif length(cell.domain) == 1
+        for (bunch, my_dict) ∈ view;
+            for (cellkey, cell) ∈ my_dict;
+                if length(cell.domain) == 1
                     cell.value = getindex(collect(cell.domain),1); # assign the value if not already done
-                    for (j_key, j_cell) ∈ my_dict
-                        if cell.index != j_cell.index
+                    cell.fixed = true;
+                    for (j_key, j_cell) ∈ my_dict;
+                        if cell.index != j_cell.index;
                              delete!(j_cell.domain, cell.value); # remove a known value from other domains if not already done
                         end
                     end
@@ -77,26 +142,48 @@ check = function(state)
             end
         end
     end
-    push!(soln, s);
-    return soln # return the updated state,wrapped in an array
-end
+    return s; # return the updated state,wrapped in an array
+end;
+
+check = function(state)
+    for view ∈ [state.rows, state.columns, state.boxes];
+        # viewkey is "rows", "columns", or "boxes"
+        # view is a dictionary of a dict of cells (row, (row, col))
+        for (bunch, my_dict) ∈ view;
+            for (cellkey, cell) ∈ my_dict;
+                if length(cell.domain) == 0
+                    return false;
+                end
+            end
+        end
+    end
+    return true    
+end;
 
 # see https://github.com/JuliaLang/julia/issues/14672 - fix implemented from v0.7.0 onwards
-Base.isless(p::Pair, q::Pair) =
-           ifelse(!isequal(p.second,q.second),
-               isless(p.second,q.second),
-               isless(p.first,q.first)
-           );
+# Base.isless(p::Pair, q::Pair) =
+#            ifelse(!isequal(p.second,q.second),
+#                isless(p.second,q.second),
+#                isless(p.first,q.first)
+#            );
 
-function hypothesise(state; strategy = "smallest_domains_first")
+function hypothesise(state; strategy = "smallest_domains_first", verboselevel = 0)
     # given a puzzle ste, use the given strategy to identify a cell.
     # for every possible value of that cell, create a new puzzle with that value filled in
-    states = Array{Dict{String, Dict{Integer,Dict{Tuple{Integer, Integer}, Cell}}},1}(); # array of all new states after hypothesis
+    states = Array{Sudoku,1}(); # array of all new states after hypothesis
+    updated_state = update(state)
+    if !check(updated_state)
+        if verboselevel > 0
+            println("I reached an invalid state!")
+            print(updated_state)
+        end
+        return states
+    end
     
     # get the lengths of all cell domains
     # arbitrarily iterate by rows
     domain_lengths = Dict{Tuple{Int, Int, Int, Int}, Int}();
-    for (bunch, my_dict) ∈ state["rows"]
+    for (bunch, my_dict) ∈ updated_state.rows
         for (cellkey, cell) ∈ my_dict
             domain_lengths[(cell.rowid, cell.colid, cell.boxid, cell.index)] = length(cell.domain);
         end
@@ -106,8 +193,11 @@ function hypothesise(state; strategy = "smallest_domains_first")
         return states
     elseif all(i -> i[2] == 1, domain_lengths)
         #then we are at a unique solution
-        println("Found a solution!")
-        push!(states, state);
+        if verboselevel > 0
+            println("Found a solution!")
+            print(updated_state);
+        end
+        push!(states, updated_state);
         return states
     else
         # the puzzle may still have at least one solution
@@ -117,9 +207,9 @@ function hypothesise(state; strategy = "smallest_domains_first")
             #println(multi_domains)
             println(domain_lengths);
             println(values(domain_lengths));
-            #println(state)
+            print(updated_state)
             error("How did we get here???");
-            push!(states);
+            push!(states, updated_state);
             return states;
         end
         if strategy == "smallest_domains_first"
@@ -132,23 +222,27 @@ function hypothesise(state; strategy = "smallest_domains_first")
             error("Bad spelling of strategy name");
         end
         
-        chosen_cell = state["rows"][chosen_cell_id[1][1]][(chosen_cell_id[1][1], chosen_cell_id[1][2])];
+        chosen_cell = updated_state.rows[chosen_cell_id[1][1]][(chosen_cell_id[1][1], chosen_cell_id[1][2])];
         chosen_row = chosen_cell.rowid;
         chosen_col = chosen_cell.colid;
         chosen_box = chosen_cell.boxid;
         chosen_index = chosen_cell.index;
         for poss ∈ chosen_cell.domain
             # create a copy of the current state
-            new_state = deepcopy(state);
+            new_state = deepcopy(updated_state);
             # update the chosen cell to the chosen possibility
-            new_cell = Cell(Set(poss), chosen_row, chosen_col, chosen_index, poss);
-            push!(new_state["rows"][chosen_row], (chosen_row, chosen_col) => new_cell);
-            push!(new_state["columns"][chosen_col], (chosen_row, chosen_col) => new_cell);
-            push!(new_state["boxes"][chosen_box], (chosen_row, chosen_col) => new_cell);
-            new_states = check(new_state);
-            if length(new_states) > 0
+            new_cell = Cell(Set(poss), chosen_row, chosen_col, chosen_index, poss, false, true);
+            push!(new_state.rows[chosen_row], (chosen_row, chosen_col) => new_cell);
+            push!(new_state.columns[chosen_col], (chosen_row, chosen_col) => new_cell);
+            push!(new_state.boxes[chosen_box], (chosen_row, chosen_col) => new_cell);
+            if verboselevel > 0
+                println("Trying a new hypothesis:")
+                print(new_state)
+            end
+            new_states = update(new_state);
+            if check(new_states)
                 # then not yet invalid
-                my_solutions = hypothesise(new_states[1]; strategy = strategy);
+                my_solutions = hypothesise(new_states; strategy = strategy, verboselevel = verboselevel - 1);
                 if length(my_solutions) > 0
                     append!(states, my_solutions)
                 end
@@ -156,8 +250,15 @@ function hypothesise(state; strategy = "smallest_domains_first")
         end
     end
     return states
-end
-n = check(sudoku);
+end;
+#n = check(sudoku);
 
-using Gallium
-a = hypothesise(sudoku);
+#using Gallium
+p = Sudoku(puzzle);
+println("Received sudoku:")
+print(p)
+println("Ready to solve!")
+a = hypothesise(p);
+for i ∈ a   
+    print(i, "\n");
+end
